88446ffb741711a72ed28eb1ab1f060a301fb0c0
Timothy Baldridge <tbaldridge@gmail.com>
2013-11-07 15:23:57 -0700

    updated deps

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :plugins [[lein-cljsbuild "1.0.0-alpha2"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-		 [core.async "0.1.0-SNAPSHOT"]
+		 [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]
                  [http-kit "2.1.10"]
                  [cheshire "5.2.0"]
-                 [org.clojure/clojurescript "0.0-1913"]
-                 [com.cemerick/austin "0.1.1"]]
+                 [org.clojure/clojurescript "0.0-2014"]
+                 [com.cemerick/austin "0.1.3"]]
   :profiles {:dev {:repl-options {:init-ns user}
                    :plugins [[com.cemerick/austin "0.1.0"]
                              [lein-cljsbuild "0.3.2"]]
